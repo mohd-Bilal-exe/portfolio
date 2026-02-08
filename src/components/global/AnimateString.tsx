b33e70beb4001a4c4b05edda6ed0e3e0ab186358
Mohd Bilal <bilalmail7054@gmail.com>
2026-02-08 19:35:05 +0530

    Added optimization to Aniamted strings

--- src/components/global/AnimateString.tsx
+++ src/components/global/AnimateString.tsx
@@ -1,48 +1,70 @@
 import { motion, useReducedMotion } from 'framer-motion';
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.04,
+    },
+  },
+};
+
 const AnimateString = ({
   children,
   className,
+  direction = 'bottom',
   delayOffset = 0,
 }: {
   children: string;
   className?: string;
+  direction?: 'top' | 'bottom';
   delayOffset?: number;
 }) => {
   if (!children) return null;
-  const words = children.split(' ').filter(Boolean);
+
   const shouldReduce = useReducedMotion();
+  const words = children.split(' ').filter(Boolean);
+
   if (shouldReduce) {
-    return (
-      <motion.span
-        className="inline-block mr-3 last:mr-0 transform-gpu"
-        initial={{ opacity: 0, y: 15, filter: 'blur(5px)' }}
-        whileInView={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5 }}
-      >
-        {children}
-      </motion.span>
-    );
+    return <span className={className}>{children}</span>;
   }
+  const wordVariant = (direction: 'top' | 'bottom') => ({
+    hidden: {
+      opacity: 0,
+      y: direction === 'top' ? -10 : 10,
+      filter: 'blur(10px)',
+    },
+    visible: {
+      opacity: [0, 0.5, 1],
+      y: [direction === 'top' ? -10 : 10, direction === 'top' ? 5 : -5, 0],
+      filter: ['blur(10px)', 'blur(5px)', 'blur(0px)'],
+    },
+  });
+
   return (
-    <span className={className}>
+    <motion.span
+      className={className}
+      variants={container}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+    >
       {words.map((word, i) => (
         <motion.span
           key={i}
           className="inline-block mr-3 last:mr-0 transform-gpu"
-          initial={{ opacity: 0, y: 15, filter: 'blur(5px)' }}
-          whileInView={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
-          viewport={{ once: true }}
-          transition={
-            shouldReduce
-              ? { duration: 0 } // or skip motion entirely
-              : { duration: 0.5, delay: delayOffset + i * 0.02 }
-          }
+          variants={wordVariant(direction)}
+          transition={{
+            duration: shouldReduce ? 0 : 0.5,
+            ease: [0.22, 1, 0.36, 1],
+            delay: shouldReduce ? 0 : delayOffset + i * 0.02,
+          }}
         >
           {word}
         </motion.span>
       ))}
-    </span>
+    </motion.span>
   );
 };
+
 export default AnimateString;
